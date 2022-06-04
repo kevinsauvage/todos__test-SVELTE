ca38100b7fc48af8e80947e1b1c9607ccae0ef2d
Kevin Sauvage <kevinsauvage@outlook.com>
2022-06-04 20:48:02 +0200

    refactoring code

--- src/lib/stores/todo.store.js
+++ src/lib/stores/todo.store.js
@@ -4,21 +4,21 @@ export const todos = writable([]);
 export const toUpdateTodo = writable("");
 
 export const handleAddTodo = (todo) => {
-  todos.update((todos) => [
-    { task: todo, done: false, id: todos?.[0] ? Number(todos?.[0].id) + 1 : 1 },
+  return todos.update((todos) => [
+    { task: todo, done: false, id: todos?.[0] ? Number(todos[0].id) + 1 : 1 },
     ...todos,
   ]);
 };
 
 export const handleToggle = (id) => {
-  todos.update((todos) =>
+  return todos.update((todos) =>
     todos.map((todo) => (Number(todo.id) === Number(id) ? { ...todo, done: !todo.done } : todo))
   );
 };
 
 export const handleDelete = (id) => {
-  todos.update((todos) => {
-    const newTodos = todos.filter((todo) => Number(todo.id) != Number(id));
+  return todos.update((todos) => {
+    const newTodos = todos.filter((todo) => Number(todo.id) !== Number(id));
     if (!newTodos.length) window.localStorage.setItem("todos", []);
     return newTodos;
   });
@@ -28,5 +28,5 @@ export const updateTodo = (task, id) => {
   todos.update((todos) =>
     todos.map((todo) => (Number(todo.id) === Number(id) ? { ...todo, task } : todo))
   );
-  toUpdateTodo.update(() => "");
+  return toUpdateTodo.update(() => "");
 };
